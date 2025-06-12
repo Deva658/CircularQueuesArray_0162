7026a81790b08aa6e5d24bbe2da7f4bf92ace1b2
Deva658 <devaaditya271@gmail.com>
2025-06-12 10:46:19 +0700

    perbaiki logika iterasi dalam metode display untuk menampilkan elemen antrian di kelas Queues

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -111,7 +111,7 @@ public:
             while (FRONT_position == REAR_position)
             {
                 cout << queue_array[FRONT_position] << "  ";
-                FRONT_position==;
+                FRONT_position++;
             }
             cout << endl;
         }
